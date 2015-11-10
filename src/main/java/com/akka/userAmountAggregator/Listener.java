7672812d9406a4ea5ba4156b5ae5db6e9e0eb67a
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2015-11-10 15:15:04 +0200

    add output to file

--- src/main/java/com/akka/userAmountAggregator/Listener.java
+++ src/main/java/com/akka/userAmountAggregator/Listener.java
@@ -2,17 +2,11 @@ package com.akka.userAmountAggregator;
 
 import akka.actor.UntypedActor;
 
-import java.math.BigDecimal;
-import java.util.Map;
-
 public class Listener extends UntypedActor {
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof App.AmountAggregation) {
             App.AmountAggregation amountAggregation = (App.AmountAggregation) message;
-            for (Map.Entry<Long, BigDecimal> longBigDecimalEntry : amountAggregation.getUserIdToAmount().entrySet()) {
-                System.out.println("id:" + longBigDecimalEntry.getKey() + ", amount:" + longBigDecimalEntry.getValue());
-            }
             System.out.println(String.format("Calculation time: \t%s", amountAggregation.getDuration()));
             getContext().system().shutdown();
         } else {
--- src/main/java/com/akka/userAmountAggregator/Master.java
+++ src/main/java/com/akka/userAmountAggregator/Master.java
@@ -8,6 +8,8 @@ import akka.util.Duration;
 import com.google.common.collect.Lists;
 import com.google.common.math.IntMath;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -27,12 +29,13 @@ public class Master extends UntypedActor {
     private final int numberOfElements;
     private int numberOfResults;
 
-    private final String PATH = "D:\\java\\Projects\\UserAmountAggregator\\src\\main\\resources\\test.txt";
+    private final File INPUT_FILE = new File("src/main/resources/test.txt");
+    private final File OUTPUT_FILE = new File("src/main/resources/output.txt");
     private final ActorRef listener;
 
     private final ActorRef workerRouter;
 
-    private Map<Long, BigDecimal> userIdToAmount = new HashMap<Long, BigDecimal>();
+    private Map<Long, BigDecimal> userIdToAmount = new HashMap<>();
 
     public Master(int numberOfWorkers, int numberOfMessages, int numberOfElements, ActorRef listener) {
         this.numberOfWorkers = numberOfWorkers;
@@ -47,7 +50,6 @@ public class Master extends UntypedActor {
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof App.Calculate) {
-            //read the file
             List<String> file = getFile();
 
             int partitionSize = IntMath.divide(file.size(), numberOfMessages, RoundingMode.UP);
@@ -68,6 +70,7 @@ public class Master extends UntypedActor {
             }
             numberOfResults += 1;
             if (numberOfResults == numberOfMessages) {
+                resultMapToFile(userIdToAmount);
                 Duration duration = Duration.create(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
                 listener.tell(new App.AmountAggregation(userIdToAmount, duration), getSelf());
                 getContext().stop(getSelf());
@@ -77,10 +80,21 @@ public class Master extends UntypedActor {
         }
     }
 
+    private void resultMapToFile(Map<Long, BigDecimal> resultMap){
+        try (FileWriter writer = new FileWriter(OUTPUT_FILE)){
+            for (Map.Entry<Long, BigDecimal> longBigDecimalEntry : resultMap.entrySet()) {
+                writer.write(String.format("\"%d;%s\";%n", longBigDecimalEntry.getKey(), longBigDecimalEntry.getValue().toString()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     private List<String> getFile() {
         List<String> lines = null;
         try {
-            lines = Files.readAllLines(Paths.get(PATH), StandardCharsets.UTF_8);
+            lines = Files.readAllLines(Paths.get(INPUT_FILE.getAbsolutePath()), StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
